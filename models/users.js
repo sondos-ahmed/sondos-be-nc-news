5202798ae46ab9f8a53eddfff0f50a1b2470e4de
sondos-ahmed <sondos.betamer@gmail.com>
2022-11-17 10:04:59 +0000

    remove console log

--- models/users.js
+++ models/users.js
@@ -1,10 +1,7 @@
 const db = require("../db/connection");
 
 exports.selectAllUsers = () => {
-  console.log("rows");
-
   return db.query("SELECT * FROM users;").then(({ rows }) => {
-    console.log(rows);
     return rows;
   });
 };
